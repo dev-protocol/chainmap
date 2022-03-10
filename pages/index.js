28c6c1c261dad802c62722b56b40b489b1daacfa
BrianObilo <brianobie91@gmail.com>
2022-03-11 00:21:56 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -167,6 +167,8 @@ function Home({ changeTheme, theme, sortedChains }) {
                 <Typography className={classes.buttonLabel}>Add Your Network</Typography>
               </Button>
               <p>
+              <p>
+              <p>
               <Typography className={classes.subTitle}>
                 Chainslist has been deployed, secured and actively maintained by TheMind
               </Typography>
